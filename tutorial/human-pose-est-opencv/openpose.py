7266ff5bc5d95d8d01287b0939730960df9845c5
Gonron <lundsgaard1997@hotmail.dk>
2019-05-09 15:44:03 +0200

    added text on the keypoints

--- tutorial/human-pose-est-opencv/openpose.py
+++ tutorial/human-pose-est-opencv/openpose.py
@@ -66,10 +66,18 @@ while cv.waitKey(1) < 0:
         idFrom = BODY_PARTS[partFrom]
         idTo = BODY_PARTS[partTo]
 
+        font = cv.FONT_HERSHEY_SIMPLEX
+        fontColor = (0, 0, 255)
+
         if points[idFrom] and points[idTo]:
             cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            
             cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
             cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+    
+            cv.putText(frame, str(partTo), points[idTo], font, 0.5, fontColor, 1, cv.LINE_AA)
+            cv.putText(frame, str(partFrom), points[idFrom], font, 0.5, fontColor, 1, cv.LINE_AA)
+            
 
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
