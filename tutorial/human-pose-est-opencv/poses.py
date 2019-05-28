2ce1e1061133132e6c7f84287ef224300a6c5f18
mortenBB <mortenbb1@gmail.com>
2019-05-28 11:37:17 +0200

    poses updated

--- tutorial/human-pose-est-opencv/poses.py
+++ tutorial/human-pose-est-opencv/poses.py
@@ -1,23 +1,33 @@
-def tpose(y):
+# y[3] == right elbow
+# y[6] == left elbow
+# y[1] == neck
+def tpose(y,x):
+    points =0
     if len(y) <= 5:
         res = 'To Few Points Detectet'
-    elif y[2] and y[4] == y[0] or y[2] and y[4] > y[0]*1.1:
-        res = 'This is a perfect t-pose!'
+    elif y[3] and y[6] == y[1] or (y[3] and y[6] > y[1]*1.2) and (y[3] and y[6] < y[1]*0.8):
+        res = "this is at perfect pse"
+        if x[11] - x[8] < 50:
+            res = 'This is a perfect t-pose!'
+            points = points+100
+        else:
+            res = "You got the arms but remember your legs!"
+            points = points+50
     else:
         res ="Try again sonnyboy"
     return res
 
-def ypose(y):
+def ypose(y,x):
     if len(y) <= 5:
         res = 'To Few Points Detectet'
-    elif y[2] and y[4] > y[0]*1.3:
+    elif (y[3] and y[6] < y[1]*0.9) and (y[3] and y[6] > y[1]*0.7):
           res ="This is a perfect y-pose!"
     else:
         res ="Try again sonnyboy"
     return res
     
-def ipose(xs):
-    if xs[2] and xs[4] < xs[0] *1.2:
+def ipose(y,x):
+    if x[2] and x[4] < x[0] *1.2:
         return print("Success")
     else:
         return print("Fail")
